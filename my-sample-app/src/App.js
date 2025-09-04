29992830538d40e4b9ce63b1f98afc31f7cf1ab8
PallaviPrasad <61450900+pallavi-jtrainer@users.noreply.github.com>
2025-09-04 14:16:07 +0530

    create react app demo

---
+++ my-sample-app/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h3><center>Welcome to react!!</center></h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
